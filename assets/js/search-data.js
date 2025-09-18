88ba5b603f1fd16f72ef7b874b8f2a0ca0cb53fb
geraldhinson <geraldlhinson@gmail.com>
2025-09-18 06:50:56 +0000

    Deploying to gh-pages from @ geraldhinson/geraldhinson.github.io@f0809ee190d6e9a743d8eec3b2a5733f347479d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/faith/";
           },
+        },{id: "nav-projects-examples",
+          title: "projects-examples",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects-examples/";
+          },
         },{id: "nav-projects",
           title: "projects",
-          description: "A fun (er, empty!) collection of projects I&#39;ve worked on (I will update this after adding a bit to my blog).",
+          description: "Various projects I&#39;ve worked on in the past.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -345,17 +352,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/ItsNotTheProcess/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
@@ -407,6 +403,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-david-vs-goliath",
+          title: 'David vs Goliath',
+          description: "A storytelling / game experience for kids",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/David%20vs%20Goliath/";
             },},{
         id: 'social-email',
         title: 'email',
